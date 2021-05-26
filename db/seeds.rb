2027a22b03ec6a0c63194686752542a1a7cb9243
Benjamin Coccoli <benji.coccoli@gmail.com>
2021-05-26 17:02:54 -0300

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database.."
+
+Restaurant.destroy_all
+
+puts "Creating new restaurants.."
+
+5.times do
+  new_restaurant = Restaurant.new(
+    name: Faker::Kpop.boy_bands,
+    address: Faker::Movies::StarWars.planet,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample,
+    phone_number: rand(10**10)
+    )
+
+ new_restaurant.save
+
+ puts "#{new_restaurant.id} - #{new_restaurant.name} restaurant - on #{new_restaurant.address} street - #{new_restaurant.category} food - The number is #{new_restaurant.phone_number}"
+
+end
+
+puts "done"
